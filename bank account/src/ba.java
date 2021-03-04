d50b0246468956333e7883086fa2b10ab526fc72
nicoleczerepak <nicoleczerepak@localhost>
2021-03-03 19:05:33 -0500

    updated bank account

--- bank account/src/ba.java
+++ bank account/src/ba.java
@@ -2,6 +2,8 @@
 public class ba 
 {
 	   private double balance;
+	   
+	   private double interestPct;
 	  
 	   
 	  
@@ -9,8 +11,11 @@ public class ba
 	      Constructs a bank account with zero balance.
 	   */
 	   public ba(double amount)
+	   public ba(double amount, double intpct)
 	   {
 	      balance = amount;
+	      interestPct = intpct;
+	      System.out.println( "  Created new account with " + amount + " and interest rate" + interestPct );
 	   }
 
 	   /**
@@ -20,20 +25,10 @@ public class ba
 	   public void deposit(double amount)
 	   {
 	      balance = balance + amount;
-	      
-	      
-	   }
-
-	   /**
-	      Makes a withdrawal from this account, or charges a penalty if
-	      sufficient funds are not available.
-	      @param amount the amount of the withdrawal
-	   */
-	   public void withdraw(double amount)
-	   {
-	      balance = balance - amount;
+	      System.out.println("  Deposited:  " + amount);
 	      
 	   }
+	  
 	   /**
 	      Gets the current balance of this bank account.
 	      @return the current balance
@@ -41,5 +36,30 @@ public class ba
 	   public double getBalance()
 	   {
 		   return balance;
+	   }
+	   public void calcInterest() {
+		   double  interst = balance * interestPct;
+		   balance = balance + interst;
+		   System.out.println("   Interest: " + interst);
+		   
+	   }
+
+/**
+	      Makes a withdrawal from this account, or charges a penalty if
+	      sufficient funds are not available.
+	      @param amount the amount of the withdrawal
+	   */
+	   public void withdraw(double amount)
+	   {
+	      balance = balance - amount;
+	      if (amount > balance) {
+	    	  System.out.println("Insufficient funds, please try again. ");
+	      }
+	      else {
+	    	  balance = balance - amount;
+	    	  System.out.println(" Withdrew: " + amount);
+	     
+	      }
+	      
 	   }
 	}
--- bank account/src/ba2.java
+++ bank account/src/ba2.java
@@ -4,18 +4,38 @@ public class ba2 {
 	public static void main(String[] args) {
 			Scanner in = new Scanner(System.in);
 			//
-			System.out.println("Enter the starting balance of your bank account: ");
+			//System.out.println("Enter the starting balance of your bank account: "); //del
+			System.out.println("Enter an amount to start the account: ");
+			double startBalanc = in.nextDouble();
 			
-			ba BankAccount = new ba(in.nextDouble());
+			System.out.println("Enter the interest rate for the account: ");
+	 		double interestPct = in.nextDouble();
+	 		
+	 		ba myBankAccount = new ba(startBalanc, interestPct);
 			
-			System.out.println("Enter the amount you would like to withdraw?: ");
+			//ba BankAccount = new ba(in.nextDouble()); //del
 			
-			BankAccount.withdraw(in.nextDouble());
+			System.out.println("Enter the amount you would like to withdraw?: "); //del
+			//BankAccount.withdraw(in.nextDouble());   //del
+			System.out.println("Enter an amount to withdraw from the account: ");
+			double withdrawAmount = in.nextDouble();
+			myBankAccount.withdraw(withdrawAmount);
 			
-			System.out.println("Enter the amount you would like to deposit: ");
+			//System.out.println("Enter the amount you would like to deposit: "); //del
+			System.out.println("Enter amount to deposit in the account: ");
+			double depositAmount = in.nextDouble();
+			myBankAccount.deposit(depositAmount);
 			
-			BankAccount.deposit(in.nextDouble());
+			BankAccount.deposit(in.nextDouble());//del
 			
-			System.out.println("The balance of your account is $" + BankAccount.getBalance());
+			//System.out.println("The balance of your account is $" + BankAccount.getBalance()); //del
+			
+			System.out.println(" If you want me to generate interest, enter 'Yes': ");
+			String ansr = in.next();
+			if (ansr.equals("Yes")) {
+				myBankAccount.calcInterest();
+			}
+			
+			System.out.println ("Your balance is: " + myBankAccount.getBalance());
 	}
 }
